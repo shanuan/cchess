dd4cb3ebcef7de910eba002de8a81eef8e4b44ee
littleflute <13699175164@sina.cn>
2017-09-24 14:34:50 +0800

    Update board.js
    Board.prototype.flipped = function(sq) {
    //  return this.computer == 0 ? SQUARE_FLIP(sq) : sq;
      return （this.computer == 0 || this.computer == 3） ? SQUARE_FLIP(sq) : sq; //by xd 14:34 2017/9/24 bjt
    }

--- play/board.js
+++ play/board.js
@@ -137,7 +137,8 @@ Board.prototype.setSearch = function(hashLevel) {
 }
 
 Board.prototype.flipped = function(sq) {
-  return this.computer == 0 ? SQUARE_FLIP(sq) : sq;
+//  return this.computer == 0 ? SQUARE_FLIP(sq) : sq;
+  return （this.computer == 0 || this.computer == 3） ? SQUARE_FLIP(sq) : sq; //by xd 14:34 2017/9/24 bjt
 }
 
 Board.prototype.computerMove = function() {
